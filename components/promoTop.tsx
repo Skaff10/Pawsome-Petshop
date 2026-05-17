9d1e0e8032399ba190c1634f33648dfe882fdcb2
Skaff10 <str.skaff@gmail.com>
2026-05-18 00:16:40 +0600

    font added & promoTop.tsx file created

---
+++ components/promoTop.tsx
@@ -0,0 +1,14 @@
+const promoTop = () => {
+  return (
+    <div className="flex items-center justify-between bg-[#9DBF9E]">
+      <div></div>
+      <h1 className="font-extrabold text-center text-[#F0EBD8] font-nunito py-2 text-xs md:text-sm">
+        GET IT FAST!! <br />
+        With free delivery on orders $39+
+      </h1>
+      <div></div>
+    </div>
+  );
+};
+
+export default promoTop;
